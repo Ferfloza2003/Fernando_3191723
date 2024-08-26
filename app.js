d001aaa7cf3c809cd5680e03e3b3778533788b00
Ferfloza2003 <fernandoflores66@aragon.unam.mx>
2024-08-26 13:33:47 -0600

    Tarea

---
+++ app.js
@@ -0,0 +1,5 @@
+let nombre = prompt('Mi nombre es Fernando y el tuyo?');
+let edad = parseInt (prompt ('Mi edad es 21 y la tuya?'));
+
+console.log('El nombre es:', nombre);
+console.log('La edad es', edad);
